0fa1f8dab55d54b369288da05da25e37a2df90bb
QueenTesa Fuggett <qfuggett@gmail.com>
2020-09-23 21:40:46 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -9,22 +9,23 @@ class Author
   end
   
   
-  def add_post(actual_post)
-    self.actual_post << actual_post
-    actual_post.author = self        #associates song with artist
+  def add_post(posts)
+    self.posts << posts
+    posts.author = self        #associates song with artist
     @@post_count += 1 
   end
   
   
   def posts
-    Post.all.select {|posts| actual_post.author == self}
+    Post.all.select {|posts| posts.author == self}
   end
   
   
   def add_post_by_title(title)
-    actual_post = Post.new(title)
-    @posts << actual_post
-    actual_post.author = self
+    posts = Post.new(title)
+    @posts << posts
+    posts.author 
+    add_post = posts
     @@post_count += 1
   end
   
